066fa320e5df53489f9811b27032256dbe6d99e1
Lior Zimmerman <liorz1984@gmail.com>
2017-10-02 23:17:21 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='basicmvc',
+        author="Lior Zimmerman",
+        author_email="liorz1984@gmail.com",
+        version='0.1',
+        packages=['basicmvc',],
+        license='MIT License',
+        long_description=open('README').read()
+)
