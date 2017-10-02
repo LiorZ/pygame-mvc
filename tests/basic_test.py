6720b7448bc913bd5e12f01a11bc2000eb8a9ba2
Lior Zimmerman <liorz1984@gmail.com>
2017-10-02 20:12:42 +0300

    Added basic test

---
+++ tests/basic_test.py
@@ -0,0 +1,10 @@
+import unittest
+from ... import model,view,controller
+
+class BasicTest(unittest.TestCase):
+
+    def test_model_creation(self):
+        new_model = model.Model()
+
+if __name__ == '__main__':
+        unittest.main()
